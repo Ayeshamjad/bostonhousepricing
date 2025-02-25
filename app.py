77b1023958e629cd31f33a1289edc5a1ae277be6
ayeshaamjad016@gmail.com <ayeshaamjad016@gmail.com>
2025-02-26 02:01:20 +0500

    api  commit

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+
 ## Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
+
+
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
@@ -25,11 +28,11 @@ def predict_api():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    output = regmodel.predict(final_input)[0]
+    
+    return render_template("home.html", prediction_text=f"The House price prediction is {output}")
 
 
 
